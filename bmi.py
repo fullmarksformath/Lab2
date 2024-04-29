f6bda5c36d0c7f2bd16cb09b1ac91a9f43b43303
Matthew <matthew.23@ichat.sp.edu.sg>
2024-04-29 15:18:49 +0800

    BMI Updated

--- bmi.py
+++ bmi.py
@@ -1,3 +1,5 @@
+
+
 def calculate_bmi(height, weight):
     print("Height= " + str(height))
     print("Weight= " + str(weight))
@@ -8,10 +10,13 @@ def calculate_bmi(height, weight):
 
     if BMI<18.5:
         print("bro EAT more NIGGA ")
+        return -1
     elif BMI>=18.5 and BMI<=25.0:
         print("ok bro ure good")
+        return 0
     elif BMI>25.0:
         print("ni zhe ge si hei ren kuai dian qu gym bro")
+        return 1
 
 calculate_bmi(weight=53, height=1.73)
 
